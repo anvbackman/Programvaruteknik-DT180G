7a0dbf9367f5a621d99b094cc5ae43d58a224f12
Andreas Backman <anba2205@student.miun.se>
2023-05-30 20:05:36 +0200

    y

--- Project/src/main/java/com/dt180g/project/characters/heroes/BaseHero.java
+++ Project/src/main/java/com/dt180g/project/characters/heroes/BaseHero.java
@@ -1,17 +1,9 @@
 package com.dt180g.project.characters.heroes;
 
-import com.dt180g.project.abilities.BaseAbility;
 import com.dt180g.project.characters.BaseCharacter;
-import com.dt180g.project.characters.CharacterEquipment;
 import com.dt180g.project.characters.CharacterStats;
-import com.dt180g.project.gear.Armor;
-import com.dt180g.project.gear.BaseGear;
 import com.dt180g.project.gear.GearManager;
-import com.dt180g.project.gear.Weapon;
 import com.dt180g.project.support.ActivityLogger;
-import com.dt180g.project.support.AppConfig;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseHero extends BaseCharacter {
@@ -33,49 +25,11 @@ public abstract class BaseHero extends BaseCharacter {
         for (String armorType : GearManager.INSTANCE.getAllMappedArmorPieces().keySet()) {
             getEquipment().addArmorPiece(armorType, GearManager.INSTANCE.getRandomArmorOfType(armorType, equip));
         }
-
-
-
-//        if (equip.equals(Weapon.class)) {
-//
-//            Weapon twoHandedWeapon = gearManager.getRandomWeapon(equip);
-//            Weapon oneHandedWeapon = gearManager.getRandomOneHandedWeapon(equip);
-//
-//
-//            if (twoHandedWeapon != null) {
-//                characterEquipment.addWeapon(twoHandedWeapon);
-//            }
-//            else if (oneHandedWeapon != null) {
-//                characterEquipment.addWeapon(oneHandedWeapon);
-//                characterEquipment.addWeapon(oneHandedWeapon);
-//
-//            }
-
-
-
-
-
-//        if (equip.equals(Armor.class)) {
-//
-//            Armor chest = gearManager.getRandomArmorOfType("Chest", equip);
-//            Armor hands = gearManager.getRandomArmorOfType("Hands", equip);
-//            Armor head = gearManager.getRandomArmorOfType("Head", equip);
-//            Armor feet = gearManager.getRandomArmorOfType("Feet", equip);
-//            Armor legs = gearManager.getRandomArmorOfType("Legs", equip);
-//
-//            characterEquipment.addArmorPiece("Chest", chest);
-//            characterEquipment.addArmorPiece("Hands", hands);
-//            characterEquipment.addArmorPiece("Head", head);
-//            characterEquipment.addArmorPiece("Feet", feet);
-//            characterEquipment.addArmorPiece("Legs", legs);
-//        }
-
-
     }
 
     public void resetHeroStats() {
         CharacterStats characterStats = getCharacterStats();
-
+        
         characterStats.resetActionPoints();
         characterStats.resetHitPoints();
         characterStats.resetEnergyLevel();
